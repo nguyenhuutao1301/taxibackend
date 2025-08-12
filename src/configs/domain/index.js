e50b4c1274e2b4efcd3605ef0fc522ee8d008f45
nguyenhuutao1301 <nguyenhuutao1301@gmail.com>
2025-08-12 21:17:30 +0700

    update

--- src/configs/domain/index.js
+++ src/configs/domain/index.js
@@ -12,6 +12,21 @@ const configMap = {
     EMAIL_PASS: process.env.EMAIL_PASS_LOCAL,
     FRONTEND_NAME: "local",
     DOMAIN: process.env.DOMAIN,
+    PROMPT: `Nội dung phải CHUYÊN SÂU:
+   - bài viết dài khoảng 4000 - 4500 từ
+   - Ngành viết về dịch vụ đặt xe : xe ôm , taxi , giao hàng
+   - dùng thông tin về địa phương như các huyện , tỉnh , phường , xã , thành phố và các địa điểm nổi tiếng , các danh lam thắng cảnh để viết chuyên sâu có thể sử dụng mỗi địa phương làm 1 tiêu đề phụ
+   - Có ví dụ thực tế, số liệu, và nghiên cứu liên quan.
+   - Dùng các thẻ H2, H3, H4 để phân chia nội dung rõ ràng.
+   - Dùng thêm các icon để làm nổi bật nội dung, ví dụ: 🚗, 🏞️, 📊, 💡, 🔍.
+   - Dùng các thương hiệu tương tự như XanhSM, Grab, Be, Vinasun để phân tích nội dung.
+   - Tags phải liên quan đến nội dung và từ khóa chính, ví dụ từ khóa "grab dĩ an" sẽ có tags: ["grab dĩ an", "grab bình an","grab bình thắng","xanh sm dĩ an","grab tân đông hiệp"].
+   - Một số thông tin su dung trong content:
+      + Số điện thoại 0327883039
+      + Website: https://datxenhanh-24h.pro.vn
+      + Có thể đặt xe qua biểu mẫu trong website ( nhap diem don, diem den, loai xe , so dien thoai )
+   - nếu có thể hãy sử dụng bảng giá cước tham khảo   
+    `,
   },
   "datxetietkiem.com": {
     DATABASE_URI: process.env.MONGODB_URI_DUYHAI,
@@ -23,6 +38,21 @@ const configMap = {
     EMAIL_PASS: process.env.EMAIL_PASS_DUYHAI,
     FRONTEND_NAME: "duyhaiweb",
     DOMAIN: process.env.DOMAIN_DUYHAI,
+    PROMPT: `Nội dung phải CHUYÊN SÂU:
+   - bài viết dài khoảng 4000 - 4500 từ
+   - Ngành viết về dịch vụ đặt xe : xe ôm , taxi , giao hàng
+   - dùng thông tin về địa phương như các huyện , tỉnh , phường , xã , thành phố và các địa điểm nổi tiếng , các danh lam thắng cảnh để viết chuyên sâu có thể sử dụng mỗi địa phương làm 1 tiêu đề phụ
+   - Có ví dụ thực tế, số liệu, và nghiên cứu liên quan.
+   - Dùng các thẻ H2, H3, H4 để phân chia nội dung rõ ràng.
+   - Dùng thêm các icon để làm nổi bật nội dung, ví dụ: 🚗, 🏞️, 📊, 💡, 🔍.
+   - Dùng các thương hiệu tương tự như XanhSM, Grab, Be, Vinasun để phân tích nội dung.
+   - Tags phải liên quan đến nội dung và từ khóa chính, ví dụ từ khóa "grab dĩ an" sẽ có tags: ["grab dĩ an", "grab bình an","grab bình thắng","xanh sm dĩ an","grab tân đông hiệp"].
+   - Một số thông tin su dung trong content:
+      + Số điện thoại 0327883039
+      + Website: https://datxetietkiem.com
+      + Có thể đặt xe qua biểu mẫu trong website ( nhap diem don, diem den, loai xe , so dien thoai )
+   - nếu có thể hãy sử dụng bảng giá cước tham khảo   
+    `,
   },
   "taxinhanh247.pro.vn": {
     DATABASE_URI: process.env.MONGODB_URI_TAXINHANH,
@@ -34,6 +64,21 @@ const configMap = {
     EMAIL_PASS: process.env.EMAIL_PASS_TAXINHANH,
     FRONTEND_NAME: "taxinhanh247.pro.vn",
     DOMAIN: process.env.DOMAIN_TAXINHANH,
+    PROMPT: `Nội dung phải CHUYÊN SÂU:
+   - bài viết dài khoảng 4000 - 4500 từ
+   - Ngành viết về dịch vụ đặt xe : xe ôm , taxi , giao hàng
+   - dùng thông tin về địa phương như các huyện , tỉnh , phường , xã , thành phố và các địa điểm nổi tiếng , các danh lam thắng cảnh để viết chuyên sâu có thể sử dụng mỗi địa phương làm 1 tiêu đề phụ
+   - Có ví dụ thực tế, số liệu, và nghiên cứu liên quan.
+   - Dùng các thẻ H2, H3, H4 để phân chia nội dung rõ ràng.
+   - Dùng thêm các icon để làm nổi bật nội dung, ví dụ: 🚗, 🏞️, 📊, 💡, 🔍.
+   - Dùng các thương hiệu tương tự như XanhSM, Grab, Be, Vinasun để phân tích nội dung.
+   - Tags phải liên quan đến nội dung và từ khóa chính, ví dụ từ khóa "grab dĩ an" sẽ có tags: ["grab dĩ an", "grab bình an","grab bình thắng","xanh sm dĩ an","grab tân đông hiệp"].
+   - Một số thông tin su dung trong content:
+      + Số điện thoại 0327883039
+      + Website: https://taxinhanh247.pro.vn
+      + Có thể đặt xe qua biểu mẫu trong website ( nhap diem don, diem den, loai xe , so dien thoai )
+   - nếu có thể hãy sử dụng bảng giá cước tham khảo   
+    `,
   },
   "datxenhanh-24h.pro.vn": {
     DATABASE_URI: process.env.MONGODB_URI_DATXENHANH_24H_PRO_VN,
@@ -45,6 +90,21 @@ const configMap = {
     EMAIL_PASS: process.env.EMAIL_PASS_DATXENHANH_24H_PRO_VN,
     FRONTEND_NAME: "datxenhanh-24h.pro.vn",
     DOMAIN: process.env.DOMAIN_DATXENHANH_24H_PRO_VN,
+    PROMPT: `Nội dung phải CHUYÊN SÂU:
+   - bài viết dài khoảng 4000 - 4500 từ
+   - Ngành viết về dịch vụ đặt xe : xe ôm , taxi , giao hàng
+   - dùng thông tin về địa phương như các huyện , tỉnh , phường , xã , thành phố và các địa điểm nổi tiếng , các danh lam thắng cảnh để viết chuyên sâu có thể sử dụng mỗi địa phương làm 1 tiêu đề phụ
+   - Có ví dụ thực tế, số liệu, và nghiên cứu liên quan.
+   - Dùng các thẻ H2, H3, H4 để phân chia nội dung rõ ràng.
+   - Dùng thêm các icon để làm nổi bật nội dung, ví dụ: 🚗, 🏞️, 📊, 💡, 🔍.
+   - Dùng các thương hiệu tương tự như XanhSM, Grab, Be, Vinasun để phân tích nội dung.
+   - Tags phải liên quan đến nội dung và từ khóa chính, ví dụ từ khóa "grab dĩ an" sẽ có tags: ["grab dĩ an", "grab bình an","grab bình thắng","xanh sm dĩ an","grab tân đông hiệp"].
+   - Một số thông tin su dung trong content:
+      + Số điện thoại 0327883039
+      + Website: https://datxenhanh-24h.pro.vn
+      + Có thể đặt xe qua biểu mẫu trong website ( nhap diem don, diem den, loai xe , so dien thoai )
+   - nếu có thể hãy sử dụng bảng giá cước tham khảo   
+    `,
   },
 };
 export default configMap;
