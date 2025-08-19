5c8cea8aeec417cf41855721a7078d15b7aed115
nguyenhuutao1301 <nguyenhuutao1301@gmail.com>
2025-08-19 18:05:33 +0700

    update

--- src/helpers/openAi/callOpenAi.helppers.js
+++ src/helpers/openAi/callOpenAi.helppers.js
@@ -1,12 +1,15 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
+
 dotenv.config();
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 export default async function callGPT(prompt) {
   const res = await openai.responses.create({
-    model: "gpt-5-nano", // GPT-5-nano
+    model: "gpt-4.1-mini", // ✅ đổi sang GPT-4 mini
     input: prompt,
   });
   return res.output_text;
